c351e3d10a8e81357d9c49719bb206a0ee5c080a
Swasti-123 <86404753+Swasti-123@users.noreply.github.com>
2022-06-29 17:50:19 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 sound1="";
 sound2="";
+
+Lx=0;
+Ly=0;
+
+Rx=0;
+Ry=0;
+
 function setup(){
     canvas= createCanvas(600, 500);
     canvas.center();
@@ -22,4 +29,25 @@ function preload(){
 
 function draw(){
     image(video, 0,0, 600,500);
+}
+
+//scoreRightWrist=0;
+//scoreLeftWrist=0;
+
+function gotPoses(){
+    function gotPoses(results){
+        if (results.length > 0){
+            console.log(results);
+            //scoreRightWrist = results[0].pose.keypoints[10].score;
+            //scoreLeftWrist = results[0].pose.keypoints[9].score;
+            //console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist );
+    
+            Lx=results[0].pose.leftWrist.x;
+            Ly=results[0].pose.leftWrist.y;
+    
+            Rx=results[0].pose.rightWrist.x;
+            Ry=results[0].pose.rightWrist.y;
+    
+        }
+    }
 }
